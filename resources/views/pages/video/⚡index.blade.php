e6447d1ab4ab788d53dd628113c1ee367ed3f5ed
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-19 10:16:11 -0600

    Update ⚡index.blade.php

--- resources/views/pages/video/⚡index.blade.php
+++ resources/views/pages/video/⚡index.blade.php
@@ -2,23 +2,49 @@
 
 declare(strict_types=1);
 
+use App\Models\Video;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 new #[Title('Videos')]
-class extends Component
-{
-    //
+class extends Component {
+
+    #[Computed]
+    public function videos()
+    {
+        return Video::query()
+            ->latest()
+            ->get();
+    }
+
 };
 ?>
 
 <div>
     <div class="flex items-center justify-between">
         <flux:heading size="xl">Videos</flux:heading>
-        <flux:button wire:navigate href="{{ route('videos.create') }}" variant="primary" size="sm">Add Video</flux:button>
+        <flux:button wire:navigate href="{{ route('videos.create') }}" variant="primary" size="sm">Add Video
+        </flux:button>
     </div>
 
-    <flux:separator variant="subtle" class="my-4" />
+    <flux:separator variant="subtle" class="my-4"/>
 
-    <div></div>
+    <flux:table class="w-full">
+        <flux:table.columns>
+            <flux:table.column>Title</flux:table.column>
+            <flux:table.column></flux:table.column>
+        </flux:table.columns>
+
+        <flux:table.rows>
+            @foreach ($this->videos as $video)
+                <flux:table.row>
+                    <flux:table.cell>{{ $video->title }}</flux:table.cell>
+                    <flux:table.cell align="end">
+                        <flux:button wire:navigate href="{{ route('videos.edit', $video) }}" size="sm">Edit</flux:button>
+                    </flux:table.cell>
+                </flux:table.row>
+            @endforeach
+        </flux:table.rows>
+    </flux:table>
 </div>
